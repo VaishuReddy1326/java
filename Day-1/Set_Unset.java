02bdc069f0c87edf7a68802e408ed16f2e5a6af0
mettavaishnavi <126755990+mettavaishnavi@users.noreply.github.com>
2023-03-11 11:04:03 +0530

    Day-1 programs done

---
+++ Day-1/Set_Unset.java
@@ -0,0 +1,11 @@
+class Set_Unset
+{
+  public  static void main(String[] args)
+  {
+    int a=10;
+    if((a&1)==0)
+    System.out.println("unset");
+    else
+    System.out.println("set");
+  }
+}
