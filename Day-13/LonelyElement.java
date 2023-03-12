50f08d2939907b7624fb78c2353b73c1e98362cf
mettavaishnavi <126755990+mettavaishnavi@users.noreply.github.com>
2023-03-12 12:01:44 +0530

    Day-13 Programs Done

---
+++ Day-13/LonelyElement.java
@@ -0,0 +1,13 @@
+//program to find lonely element in an array
+import java.util.Arrays;
+public class LonelyElement {
+    public static void main(String[] args){
+        int arr[]={3,4,5,4,5};
+        int result=arr[0];
+        for(int i=1;i<arr.length;i++){
+            result=result^arr[i];
+
+        }
+        System.out.print(result);
+    }
+}
