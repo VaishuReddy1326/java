0092759e805ea029fdc26db6b367772d01c5d83c
mettavaishnavi <126755990+mettavaishnavi@users.noreply.github.com>
2023-03-13 11:50:07 +0530

    Day-3 Programs Done

---
+++ Day-3/Object.java
@@ -0,0 +1,13 @@
+public class Object {
+     void add ( int a,int b ) {
+            System.out.println(a+b);
+        }
+            public static void main(String[] args) {
+                Object ob = new Object();
+                ob.add(10,20);
+            }
+
+
+        
+     
+}
