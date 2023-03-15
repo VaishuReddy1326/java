84bd02d89c55fe97fae99cdde146ebca5535bd70
mettavaishnavi <126755990+mettavaishnavi@users.noreply.github.com>
2023-03-15 14:22:39 +0530

    Day-6 Programs Done

--- Day-6/Downward_Pattern.java
+++ Day-6/Downward_Pattern.java
@@ -1,7 +1,14 @@
+/* program to print the downward pattern
+ *****
+ ****
+ ***
+ **
+ *
+ */
 public class Downward_Pattern {
      public static void main(String[] args){
         int n=5;
-        for(int i=1;i<=n;i++){
+        for(int i=0;i<=n;i++){
             for(int j=1;j<=n-i;j++){
                 System.out.print("*");
             }
