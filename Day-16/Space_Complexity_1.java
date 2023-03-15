1e8dfbfc0743cc81ef6047cfbd5a396faa7e7601
mettavaishnavi <126755990+mettavaishnavi@users.noreply.github.com>
2023-03-15 10:12:01 +0530

    Day-16 programs Done

---
+++ Day-16/Space_Complexity_1.java
@@ -0,0 +1,8 @@
+public class Space_Complexity_1 {
+    public static void main(String[] args){
+          int n=10;
+            System.out.println(n);  
+        
+    }
+    
+}
---
+++ Day-16/Space_Complexity_n.java
@@ -0,0 +1,11 @@
+
+import java.util.Arrays;
+public class Space_Complexity_n {
+    public static void main(String[] args){
+        int n=1000;
+        int arr[]=new int[n];
+       for(int i=0;i<=n;i++)
+        System.out.println(i);
+    }
+    
+}
