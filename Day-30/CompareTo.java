5331ba767bceb17e51e38b419dd0ee59d4fcb0fc
mettavaishnavi <126755990+mettavaishnavi@users.noreply.github.com>
2023-03-28 21:47:54 +0530

    Day-30 Programs Done

---
+++ Day-30/CompareTo.java
@@ -0,0 +1,10 @@
+
+
+public class CompareTo {
+    public static void main(String[] args){
+        String str1="welcome";
+        String str2="co";
+        System.out.println(str1.compareTo(str2));
+    }
+    
+}
