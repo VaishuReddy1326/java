1dccdddee952cd062d1f0f52d5a67377aafff27f
mettavaishnavi <126755990+mettavaishnavi@users.noreply.github.com>
2023-04-17 22:35:47 +0530

    Day-46 Programs Done

---
+++ Day-46/Static.java
@@ -0,0 +1,15 @@
+public class Static {
+    public static void main(String[] args){
+        int x=20;
+        System.out.println(x);
+
+    }
+    public static void dispaly(){
+        System.out.println("display method");
+    }
+    static{
+        int x=20;
+        System.out.println("static method");
+    }
+}  
+
