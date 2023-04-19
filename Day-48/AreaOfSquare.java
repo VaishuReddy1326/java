ed53b09c4c645e34ea664650499a8dff23034a6f
mettavaishnavi <126755990+mettavaishnavi@users.noreply.github.com>
2023-04-19 20:24:08 +0530

    Day-48 Program Done

---
+++ Day-48/AreaOfSquare.java
@@ -0,0 +1,10 @@
+
+
+public class AreaOfSquare {
+    public static void main(String[] args){
+        int a=10;
+        int areaSquare=a*a;
+        System.out.println(areaSquare);
+    }
+    
+}
