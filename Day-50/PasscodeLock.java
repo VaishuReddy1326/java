e4f37cf7380c76a24d603e450e856b18482ee84c
mettavaishnavi <126755990+mettavaishnavi@users.noreply.github.com>
2023-04-23 23:00:15 +0530

    Day-50 Program Done

---
+++ Day-50/PasscodeLock.java
@@ -0,0 +1,19 @@
+public class PasscodeLock {
+    public static void main(String[] args) {
+         Lock lock = new Lock();
+         for(int i=1000;i<10000;i++) {
+            if(lock.checkPasscode(i))
+                System.out.println("passcode :"+i);
+        }
+    }
+    
+}
+class Lock {
+    int passcode = (int)Math.floor(Math.random()*9000.0+1000.0);
+    boolean checkPasscode(int code) {
+        if(passcode == code) 
+            return true;
+        return false;
+    }
+}
+
