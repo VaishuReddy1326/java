8cfb4be506f1e49e647da8aa80a3dd6bbdc5ca0a
mettavaishnavi <126755990+mettavaishnavi@users.noreply.github.com>
2023-04-26 22:24:24 +0530

    Day-52 Programs Done

---
+++ Day-52/MultiLineArgument.java
@@ -0,0 +1,11 @@
+public class MultiLineArgument {
+
+    public static void main(String[] args)  {
+
+        System.out.println(args[0]);
+        
+        System.out.println(args[1]);
+
+    }
+    
+}
---
+++ Day-52/Prime.java
@@ -0,0 +1,18 @@
+public class Prime {
+    public static void main(String[] args) {
+        for(var x : args) {
+            int num =  Integer.parseInt(x);
+        if(num < 2)
+            System.out.println("number is unvalid");
+        for(int i = 2 ; i <= (num/2) ; i++) {
+            if((num % i)==0) {
+                System.out.println("composite" + num);
+                return;
+            }
+        }
+        System.out.println("prime" + num);
+        }
+
+    }
+    
+}
