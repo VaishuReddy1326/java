26c76629d3ece75314f8e616460db8567153575f
mettavaishnavi <126755990+mettavaishnavi@users.noreply.github.com>
2023-05-15 22:05:07 +0530

    Day-72 program done

---
+++ Day-72/ArraylistVSLinkedList.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+public class ArraylistVSLinkedList {
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        list.add(43);
+        list.add('h');
+        list.add("hello");
+        LinkedList list1 = new LinkedList();
+        list1.add(12);
+        list1.add("java");
+        System.out.println(list);
+        System.out.println(list1);
+    }
+    
+}
