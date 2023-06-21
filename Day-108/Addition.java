f12d6eae99f152fe6a0157e383bf2388f09dd403
mettavaishnavi <126755990+mettavaishnavi@users.noreply.github.com>
2023-06-21 23:45:50 +0530

    Day-108 Programs Done

---
+++ Day-108/Addition.java
@@ -0,0 +1,15 @@
+
+
+public class Addition {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 34;
+        add(a,b);
+
+    }
+    
+public static int add(int a,int b){
+    int sum = a+b;
+    return sum;
+}
+}
